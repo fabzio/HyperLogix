e9d4f073f5a8d48453649a655efac2a4d3cdf12c
Fabrizio Montoya <fabriziorandall@gmail.com>
2025-05-27 10:35:21 -0500

    feat: add simulation trigger

--- client/src/features/simulation/components/AsideTab/Run.tsx
+++ client/src/features/simulation/components/AsideTab/Run.tsx
@@ -1,13 +1,100 @@
 import { Button } from '@/components/ui/button'
-import { useStartSimulation } from '../../hooks/useSimulation'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { format } from 'date-fns'
+import { CalendarIcon } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 export default function Run() {
-  const { refetch } = useStartSimulation()
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      date: {
+        from: new Date('2025-01-01'),
+        to: new Date('2025-01-08'),
+      },
+    },
+  })
+
+  const onSubmit = form.handleSubmit((data) => {
+    alert(JSON.stringify(data, null, 2))
+  })
+
   return (
     <div>
-      <Button onClick={() => refetch()} className="w-full">
-        Ejecutar
-      </Button>
+      <Form {...form}>
+        <form onSubmit={onSubmit}>
+          <FormField
+            control={form.control}
+            name="date"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Rango de simulación</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        className={cn(
+                          'w-[300px] justify-start text-left font-normal',
+                          !field.value && 'text-muted-foreground',
+                        )}
+                      >
+                        <CalendarIcon />
+                        {field.value?.from ? (
+                          field.value.to ? (
+                            <>
+                              {format(field.value.from, 'LLL dd, y')} -{' '}
+                              {format(field.value.to, 'LLL dd, y')}
+                            </>
+                          ) : (
+                            format(field.value.from, 'LLL dd, y')
+                          )
+                        ) : (
+                          <span>Elija el rango</span>
+                        )}
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      initialFocus
+                      mode="range"
+                      defaultMonth={field.value?.from}
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      numberOfMonths={2}
+                    />
+                  </PopoverContent>
+                </Popover>
+              </FormItem>
+            )}
+          />
+          <Button>Ejecutar</Button>
+        </form>
+      </Form>
     </div>
   )
 }
+
+const formSchema = z.object({
+  date: z.object({
+    from: z.date(),
+    to: z.date(),
+  }),
+})
+type FormSchema = z.infer<typeof formSchema>
