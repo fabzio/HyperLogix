c1291c74171e345ed27a255aaffd134e7249546c
AAlvaradoE <aalvaradoe@pucp.edu.pe>
2025-06-14 23:47:17 -0500

    feat: avance

--- server/src/main/java/com/hyperlogix/server/optimizer/AntColony/Ant.java
+++ server/src/main/java/com/hyperlogix/server/optimizer/AntColony/Ant.java
@@ -30,7 +30,7 @@ public class Ant {
   private Map<String, Duration> tourTime;
   private Map<String, Double> tourCost;
   private Map<Node, Path> firstPath;
-
+  
   public Ant(PLGNetwork network, Graph graph, AntColonyConfig antColonyConfig) {
     this.originalNetwork = network.clone();
     this.graph = graph;
@@ -39,10 +39,9 @@ public class Ant {
     network.getTrucksCapacity();
     resetState();
   }
-
   public Routes findSolution() {
     for (Truck truck : network.getTrucks()) {
-
+      // Create the first node representing the truck's starting location
       Stop firstNode = new Stop(
           new Node(truck.getCode(), truck.getType().toString(), NodeType.LOCATION, truck.getLocation().integerPoint()),
           graph.getAlgorithmStartDate());
@@ -296,7 +295,6 @@ public class Ant {
     }
     return availableNodes.getLast();
   }
-
   private void moveToNode(Truck truck, Stop currentNode, Stop nextNode) {
     Path path;
     if (currentNode.getNode().getType() == NodeType.LOCATION) {
@@ -304,6 +302,8 @@ public class Ant {
     } else {
       path = adjacencyMap.get(currentNode.getNode()).get(nextNode.getNode());
     }
+    
+    // Normal movement logic when no incident or incident doesn't affect movement
     this.paths.get(truck.getId())
         .add(path.points().getFirst() == currentNode.getNode().getLocation() ? path : path.reverse());
     int distance = path.length();
